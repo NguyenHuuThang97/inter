9dc674d999d4f7b62fbd1b3b7ba08ceede936041
Nguyễn Thắng <nguyenhuuthang6197@gmail.com>
2019-07-31 15:06:06 +0700

    add page admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {DetailComponent } from './detail/detail.component'
 import {LoginComponent} from './login/login.component'
+import {AdminComponent} from './admin/admin.component'
 import { ShoppingComponent } from 'src/app/shopping/shopping.component';
+// import {AddProductComponent} from './add-product/add-product.component';
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -12,10 +14,15 @@ const routes: Routes = [
     { path: 'shoppingcart', component: ShoppingComponent },
      { path: 'login', component: LoginComponent },
     // { path: 'signup', component: SignupComponent},
-    // { path: 'admin', component: AdminComponent},
+     { path: 'admin', component: AdminComponent,
+     children: [
+      //  { path: 'add', component: AddProductComponent },
+      //  { path: 'edit', component:AddProductComponent }
+     ]
+},
     
     // { path: 'edit/:id', component: EditComponent},
-    // { path: 'add', component:AddComponent}
+    // { path: 'add', component:AddProductComponent}
   ];
 
 
--- src/app/shopping/shopping.component.ts
+++ src/app/shopping/shopping.component.ts
@@ -3,8 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../service/product.service';
 import { IProduct } from 'src/app/interfaces/product.interface';
 import { Router } from '@angular/router';
-import  { ICustomer } from 'src/app/interfaces/customer.interface'
+import { ICustomer } from 'src/app/interfaces/customer.interface'
 import { FormGroup, FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
 @Component({
   selector: 'app-shopping',
   templateUrl: './shopping.component.html',
@@ -15,26 +16,30 @@ export class ShoppingComponent implements OnInit {
   result: IProduct;
   formchinh: FormGroup;
   isCheck: boolean = false;
-  items:any = [];
-  bill:any = {};
+  items: any = [];
+  bill: any = {};
+  total: any = [];
+  giachinhthuc: number = 0;
   constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit() {
+
     var data = JSON.parse(`${localStorage.getItem('huhu')}`);
-    this. items= data;  
+    this.items = data;
     console.log(this.items);
     this.items.forEach(element => {
       console.log(element);
-      
+
     });
+    this.total1();
     // this.items = this.items.map(e => e['id']).map((e,i,arr)=>arr.indexOf(e)===i && i).filter(e => this.items[e]).map(e => data[e]);
-   // console.log(this. items);
-    
+    // console.log(this. items);
+
     this.formchinh = new FormGroup({
-      username: new FormControl(),
-      email: new FormControl(),
-      phone: new FormControl(),
-      address: new FormControl()
+      username: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      email: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [Validators.required]),
+      address: new FormControl('', [Validators.required])
     })
 
     this.productService.getProduct(this.idProduct).subscribe((data) => {
@@ -57,9 +62,9 @@ export class ShoppingComponent implements OnInit {
     this.productService.remove(this.result);
   }
   mua() {
-    this. items = localStorage.getItem('huhu');
-    console.log(this. items);
-    
+    this.items = localStorage.getItem('huhu');
+    console.log(this.items);
+
     // this.productService.giohang = [];
     // console.log(this.productService.giohang);
     // this.router.navigate(['home']);
@@ -69,26 +74,45 @@ export class ShoppingComponent implements OnInit {
   //   this.productService.cart();
   //   // this.router.navigate(['home']);
   // }
-  thanhtoan(){
-   this.productService.createCustomer(this.formchinh.value).subscribe((response) =>{
-     if(response){
+  thanhtoan() {
+    this.productService.createCustomer(this.formchinh.value).subscribe((response) => {
+      if (response) {
         alert("Thanh toán thành công");
         this.router.navigate(["home"]);
-     }
-   }),err =>{
-      if(err.status === 404){
+        localStorage.clear();
+      }
+    }), err => {
+      if (err.status === 404) {
         alert(err.error.message)
       }
-   }
-   
-   this.productService.createOderDetail(this.items.element).subscribe((res) =>{
-     console.log(res);
-     
-   })
+    }
+
+    this.productService.createOderDetail(this.items.element).subscribe((res) => {
+      console.log(res);
+
+    })
 
   }
-  removePro(idProduct){
+  removePro(idProduct) {
     this.items.splice(this.items.indexOf(idProduct), 1);
-    localStorage.setItem('huhu',JSON.stringify(this.items));
+    localStorage.setItem('huhu', JSON.stringify(this.items));
+  }
+
+  total1() {
+    this.items.map(dataItem => this.total.push(dataItem.giahientai * dataItem.lengthProduct));
+    // console.log(this.total)
+    this.total.forEach(element => {
+      this.giachinhthuc += element;
+    });
+    console.log(this.giachinhthuc);
+    
+    // this.total.push(this.items.lengthProduct*this.items.giahientai);
+    // console.log(this.total);
+    // var tong = 0;
+    // this.total.forEach(element => {
+    //   tong += element
+    // });
+    // console.log(tong);
+
   }
 }
